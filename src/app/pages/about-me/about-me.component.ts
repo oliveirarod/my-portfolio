53ca7b42e5ba169d0d83ca93a925e9618f84a575
oliveirarod <rodrigo.oliveira6446@gmail.com>
2023-12-12 20:42:08 -0300

    Início do about-me

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
 
+  ngOnInit(): void { }
 }
