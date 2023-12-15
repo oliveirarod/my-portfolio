39da32ea7f13022787788d6702f873d0095437f4
oliveirarod <rodrigo.oliveira6446@gmail.com>
2023-12-15 14:32:05 -0300

    Altera nome da pasta de 'pages' para 'section' para melhorar a sintaxe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarModule } from './components/navbar/navbar.module';
-import { HomeModule } from './pages/home/home.module';
-import { SkillsModule } from './pages/skills/skills.module';
+import { HomeModule } from './sections/home/home.module';
+import { SkillsModule } from './sections/skills/skills.module';
 
 @NgModule({
   declarations: [AppComponent],
---
+++ src/app/sections/experiences/experiences.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExperiencesComponent } from './experiences.component';
+
+
+
+@NgModule({
+  declarations: [
+    ExperiencesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExperiencesModule { }
