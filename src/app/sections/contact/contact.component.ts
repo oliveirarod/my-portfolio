148a51a90f2565332b867b30c9b7556232bc230f
oliveirarod <rodrigo.oliveira6446@gmail.com>
2024-02-08 15:08:59 -0300

    Cria seção de contato

--- src/app/sections/contact/contact.component.ts
+++ src/app/sections/contact/contact.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  form!: FormGroup;
+  formControls!: string[];
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm();
+
+    this.formControls = Object.keys(this.form.controls).filter(
+      (controlName) => controlName !== 'message'
+    );
+  }
+
+createForm() {
+  this.form = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    subject: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+    message: ['', Validators.required],
+  });
+}
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      console.log(this.form);
+      return;
+    }
+
+    console.log(this.form.value);
+  }
 }
--- src/app/sections/contact/contact.module.ts
+++ src/app/sections/contact/contact.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ContactComponent } from './contact.component';
 import { SectionTitleModule } from 'src/app/components/section-title/section-title.module';
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [CommonModule, SectionTitleModule],
+  imports: [CommonModule, SectionTitleModule, ReactiveFormsModule, FormsModule],
   exports: [ContactComponent],
 })
 export class ContactModule {}
