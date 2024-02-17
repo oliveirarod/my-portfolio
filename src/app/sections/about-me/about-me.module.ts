d02039c73c8538e34e22e9170e539d74ffdf30b8
oliveirarod <rodrigo.oliveira6446@gmail.com>
2024-02-17 00:37:04 -0300

    Adiciona animações na seção de 'about-me'

--- src/app/sections/about-me/about-me.module.ts
+++ src/app/sections/about-me/about-me.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AboutMeComponent } from './about-me.component';
 import { SectionTitleModule } from 'src/app/components/section-title/section-title.module';
+import { DirectivesModule } from 'src/app/utils/directives/directives.module';
 
 @NgModule({
   declarations: [AboutMeComponent],
-  imports: [CommonModule, SectionTitleModule],
+  imports: [CommonModule, SectionTitleModule, DirectivesModule],
   exports: [AboutMeComponent],
 })
 export class AboutMeModule {}
