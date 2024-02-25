65322dd9f3fbccef3e9f7652b9b7b7d012f44fb5
oliveirarod <rodrigo.oliveira6446@gmail.com>
2024-02-25 20:13:39 -0300

    Cria o componente works

--- src/app/sections/works/works.component.ts
+++ src/app/sections/works/works.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { WorkService } from "src/app/services/work.service";
+import { WorkInterface } from 'src/app/utils/interfaces/work';
 
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
   styleUrls: ['./works.component.scss']
 })
-export class WorksComponent {
+export class WorksComponent implements OnInit{
 
+  works: WorkInterface[] = [];
+
+  constructor(
+    private workService: WorkService
+  ) {}
+
+  ngOnInit(): void {
+    this.getWorks();
+  }
+
+  // TODO: Criar tratamento de erro
+  getWorks(): void {
+    this.workService.getBlocks().subscribe((works) => {
+      this.works = works;
+    })
+  }
+
+  openWork(link: string) {
+    window.open(link, '_blank');
+  }
 }
