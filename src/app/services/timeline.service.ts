492f9f6c2919a5e707e9bd149bc6d1a8da4680ce
oliveirarod <rodrigo.oliveira6446@gmail.com>
2024-03-01 00:27:35 -0300

    Adiciona dados reais à seção 'experiences' e ajusta responsividade

--- src/app/services/timeline.service.ts
+++ src/app/services/timeline.service.ts
@@ -10,52 +10,77 @@ export class TimelineService {
   private blocks: TimelineBlock[] = [
     {
       id: 1,
-      title: 'Title',
-      isWorkExp: false,
+      title: 'Lessa Solutions',
+      isWorkExp: true,
       date: '2019',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsaratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.',
+      desc: `
+        Full-stack developer responsible for the development of custom systems at Lessa Solutions company. Used technologies: <br />
+        <br />
+        • Angular (HTML, CSS, JavaScript/TypeScript) <br />
+        • Ionic <br />
+        • Figma, Bootstrap, Angular Material and Materialize <br />
+        • Node.js and MySQL <br />
+        • Jira e Kanban
+      `,
     },
     {
       id: 2,
-      title: 'Title',
-      isWorkExp: true,
-      date: '2019',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsaratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.',
+      title: "Bachelor's Degree - Analysis and Systems Development",
+      isWorkExp: false,
+      date: '2021',
+      desc: 'My academic journey immersed me in the theoretical foundations of technology, exploring programming paradigms and algorithms. This knowledge equips me to tackle complex challenges with clarity and innovation, ensuring confidence in navigating the dynamic tech landscape.',
     },
     {
       id: 3,
-      title: 'Title',
-      isWorkExp: false,
+      title: 'Bten Brasil',
+      isWorkExp: true,
       date: '2021',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsaratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.',
+      desc: `
+        Front-end developer responsible for the development of the ChatBot system for Bten company. Used technologies: <br />
+        <br />
+        • Angular (HTML, CSS, JavaScript/TypeScript) <br />
+        • Bootstrap, Angular Material <br />
+        • Figma <br />
+        • Node.js <br />
+        • Jira, Kanban, and Scrum
+      `,
     },
     {
       id: 4,
-      title: 'Title',
+      title: 'BV Bank',
       isWorkExp: true,
-      date: '2021',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsaratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.',
+      date: '2022',
+      desc: `
+        Front-end developer responsible for the development of the Open Finance system at Votorantim Bank (BV), outsourced by CWI Software. Used technologies: <br />
+        <br />
+        • Angular (HTML, CSS, JavaScript/TypeScript) <br />
+        • Google Analytics <br />
+        • Git and BitBucket <br />
+        • Figma <br />
+        • Jira, Kanban, and Scrum <br />
+      `,
     },
     {
       id: 5,
-      title: 'Title',
+      title: 'Bradesco',
       isWorkExp: true,
-      date: '2022',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsaratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.',
+      date: '2023',
+      desc: `
+        Full-stack developer responsible for developing systems for legal entities at Bradesco Bank, outsourced by CI&T Software S/A. Used technologies: <br />
+        <br />
+        • Angular (HTML, CSS, JavaScript/TypeScript) <br />
+        • Java (Spring Boot, Maven, JMeter) <br />
+        • Cucumber <br />
+        • Adobe XD <br />
+        • Jira, Kanban, and Scrum <br />
+      `,
     },
     {
       id: 6,
-      title: 'Title',
-      isWorkExp: true,
-      date: '2023',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsaratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.',
-    },
-    {
-      id: 7,
-      title: 'Title',
+      title: 'Postgraduate Degree - Software Engineering',
       isWorkExp: false,
       date: '2023',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsaratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.',
+      desc: 'My postgraduate studies in Software Engineering provided an advanced exploration of software design, development methodologies, and system architecture. This specialized knowledge enhances my ability to engineer robust software solutions, ensuring efficiency and adaptability in the ever-evolving tech industry.',
     },
   ];
 
